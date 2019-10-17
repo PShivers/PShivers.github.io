9642040ae2419c988558d9e7d32833bf41740390
Paul Shivers <paulsshivers@gmail.com>
2019-10-17 00:14:28 -0400

    added carousel and other minor aesthetic changes

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 $(function() {
-  $('.name').click(function() {
-    $(this).fadeOut();
-  });
 
   $('.navlink').mouseenter(function() {
     $(this).animate({ right: '20px' }, 'slow');
     $(this).animate({ left: '10px' }, 'fast');
     $(this).animate({ left: '-5px' }, 'fast');
   });
+
+  $('.carousel').carousel()
+
+
+ 
 });
